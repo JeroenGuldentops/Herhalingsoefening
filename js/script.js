543ed0644d5de0c44651bb2ae1e99273549ac1ab
JeroenGuldentops <jeroen93@msn.com>
2023-11-02 19:19:19 +0100

    drawCircle en drawRect

--- js/script.js
+++ js/script.js
@@ -3,36 +3,52 @@ let colorText = 'red';
 let shapeText = 'square';
 
 function setup() {
-    createCanvas(400, 400);
-    background(220);
-    colorButton = createButton(colorText);
-    //color.size(55);
-    colorButton.position(15, 15);
-    colorButton.mousePressed(changeColor());
-    shapeButton = createButton(shapeText);
-    //shape.size(55);
-    shapeButton.position(15 + colorButton.width + 10, 15);
-    shapeButton.mousePressed(changeShape());
-  }
-  
-  function draw() {
+      createCanvas(400, 400);
+      background(220);
+}
 
-  }
+function draw() {
+      colorButton = createButton(colorText);
+      colorButton.size(55);
+      colorButton.position(15, 15);
+      shapeButton = createButton(shapeText);
+      shapeButton.size(55);
+      shapeButton.position(15 + colorButton.width + 10, 15);
 
-  function changeColor(){
-        if(colorText === 'red') {
-            colorText = 'black';
-            fill(black);
-        } else if(colorText === 'black' ) {
-            colorText = 'yellow';
-            fill(yellow);
-        } else if (colorText === 'yellow') {
-            colorText = 'red';
-            fill(red);
-        }
-  }
+      shapeButton.mousePressed(changeShape);
+      colorButton.mousePressed(changeColor);
 
-  function changeShape(){
+      if(mousePressed){
+            if(shapeText === 'square'){
+                  drawRect();
+            } else {
+                  drawCircle();
+            }
+      }
+}
+
+function changeColor(){
+
+      switch (colorText) {
+            case 'red':
+                  colorText = 'black';
+                  fill(black);
+                  break;
+            case 'black':
+                  colorText = 'yellow';
+                  fill(yellow);
+                  break;
+            case 'yellow':
+                  colorText = 'red';
+                  fill(red);
+                  break;
+      
+            default:
+                  break;
+      }
+}
+
+function changeShape() {
       switch (shapeText) {
             case 'square':
                   shapeText = 'circle';
@@ -40,9 +56,16 @@ function setup() {
             case 'circle':
                   shapeText = 'square';
                   break;
+
             default:
                   break;
       }
+}
 
-  }
-  
+function drawCircle(){
+      ellipse(mouseX, mouseY, 30);
+}
+
+function drawRect(){
+      rect(mouseX, mouseY, 30);
+}
