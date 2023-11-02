f60b48c148d80fa43bfaf58aa966f158ca40dfeb
JeroenGuldentops <jeroen93@msn.com>
2023-11-02 19:39:38 +0100

    clear function implemented

--- js/script.js
+++ js/script.js
@@ -14,9 +14,13 @@ function draw() {
       shapeButton = createButton(shape);
       shapeButton.size(55);
       shapeButton.position(15 + colorButton.width + 10, 15);
+      clearButton = createButton('clear');
+      clearButton.size(55);
+      clearButton.position(15 + colorButton.width + 10 + shapeButton.width + 10, 15);
 
       shapeButton.mousePressed(changeShape);
       colorButton.mousePressed(changeColor);
+      clearButton.mousePressed(clearCanvas);
 }
 
 function changeColor(){
@@ -49,6 +53,10 @@ function changeShape() {
             default:
                   break;
       }
+}
+
+function clearCanvas() {
+      background(220);
 }
 
 function drawCircle(){
